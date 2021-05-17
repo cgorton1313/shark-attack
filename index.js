a66a3372933c05a6e2c61b9682457914eba1d3e4
sdigiulio21 <sdigiulio21@penguinhall.org>
2021-05-17 09:58:02 -0400

    Mock Data - SD

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 
+app.get('/attacks', (req, res) => {
+  res.json([
+    ['Great White', 6],
+    ['Hammerhead', 3],
+    ['Whale Shark', 4],
+    ['Basking', 2],
+    ['Tiger Shark', 5]
+  ])
+})
+
+
 app.listen(config.app.port, () => {
     console.log('Server running on port ' + config.app.port);
 })
