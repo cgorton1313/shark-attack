80c24d0c83811e062d265555924fa2c47c26767f
jmantilla22 <jmantilla22@penguinhall.org>
2021-05-18 13:18:36 -0400

    shark data

--- sharkAttackData.js
+++ sharkAttackData.js
@@ -5,6 +5,39 @@ const util = require('util');
 async function functionName() {
     let sql = 'select * from attacks';
     let result = await getQueryData(sql);
+    console.log(result)
     return result;
 }
 
+async function getQueryData(sql) {
+    let connection = nysql.creatConnection({
+        host: '68.66.216.18',
+        user: 'penguin_sharkattack',
+        password: 'aph_sharkattack!',
+        database: 'penguinh_sharkattack'
+    });
+
+    connection.connect(function (err) {
+        if (err) {
+            console.info('error when connectin to db:', err);
+        } else {
+            console.info('Connected to database ' + config.db.database + ' as user ' + config.db.user);
+        }
+    });
+    let query = util.promisify(connection.query).bind(connection);
+
+    let result;
+    try {
+        result = await query(sql);
+    } catch (err) {
+        console.info(err);
+        result = '{Error}'
+    }
+    connection.end();
+
+    return result;
+}
+
+module.exports = {
+    getSighting
+}
