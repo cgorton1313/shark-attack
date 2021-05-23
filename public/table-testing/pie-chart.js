98f665467f8c0e279f451f7d5495b2483ae1d3b8
Craig Gorton <ncraiggorton@gmail.com>
2021-05-23 17:57:57 -0400

    add comments to pie-chart.js

--- public/table-testing/pie-chart.js
+++ public/table-testing/pie-chart.js
@@ -13,6 +13,10 @@ function drawChart() {
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Shark Type');
     data.addColumn('number', 'Attacks');
+    // TODO:    change out this mock data with real data that you get by querying the database (manually)
+    //          so, use phpmyadmin or run SQL right from VS Code to find out how many fatalites were 'Y' and
+    //          how many were 'N'. disregard any other values. then use that data below, instead of the fake stuff
+    //          you see here
     data.addRows([
         ['Great White', 6],
         ['Hammerhead', 3],
