f176cb2c109d6c001a4fe3caf070ae98e459c16d
amartins22 <amartins22@penguinhall.org>
2021-05-24 09:30:58 -0400

    js updates

--- public/table-testing/table.js
+++ public/table-testing/table.js
@@ -1,7 +1,11 @@
   google.charts.load('current', {'packages':['table']});
   google.charts.setOnLoadCallback(drawTable);
 
-  function drawTable() {
+  async function drawTable() {
+    let response = await fetch('/attacks');
+    let attackData = await response.json();
+    console.log(attackData);
+
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Species');
     data.addColumn('number', 'Year');
@@ -10,12 +14,7 @@
     data.addColumn('boolean', 'Provoked');
     data.addColumn('string', 'Injury');
     data.addColumn('boolean', 'Fatal');
-    data.addRows([
-      ['Lemon Shark', {v: 2020, f: '2020'}, 'Australia', 'Queensland', false, 'Lacerations to leg', false],
-      ['White Shark', {v: 2019, f: '2019'}, 'U.S.A', 'Rhode Island', false, 'FATAL', true],
-      ['Reef Shark', {v: 2015, f: '2015'}, 'U.S.A', 'Gulf Coast, Florida', true, 'Loss of arm and lacerations to the abdomen', false],
-      ['White Shark', {v: 2013, f: '2013'}, 'Portugal', 'Madeira', false, 'surfing accident, only board was bitten', false]
-    ]);
+    data.addRows(attackData);
 
     var table = new google.visualization.Table(document.getElementById('table_div'));
 
