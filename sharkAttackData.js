8ebe63898b2a0fbf2cb5f06559d525f616111415
jmantilla22 <jmantilla22@penguinhall.org>
2021-05-24 10:22:26 -0400

    endpoint fatality-jm

--- sharkAttackData.js
+++ sharkAttackData.js
@@ -20,7 +20,7 @@ async function getQueryData(sql) {
         user: config.db.user,
         password: config.db.password,
         database: config.db.database
-        });
+    });
 
     connection.connect(function (err) {
         if (err) {
@@ -43,6 +43,26 @@ async function getQueryData(sql) {
     return result;
 }
 
+async function numFatalities(sql) {
+    let sql = `
+select attackCount.common_name, numFatals, numAttacks, (numFatals / numAttacks) as percentFatal
+from (
+select common_name, count(attack_id) as numAttacks
+from attacks, species
+where species.species_id = attacks.species_id
+group by common_name) as attackCount
+left join
+(select common_name, count(attack_id) as numFatals
+from attacks, species
+where species.species_id = attacks.species_id
+and fatal = 'Y'
+group by common_name) as fatalCount
+on attackCount.common_name = fatalCount.common_name
+order by percentFatal desc;
+
+`
+}
+
 module.exports = {
-    getAttacks, getCountryTotalAttacks
+    getAttacks, getCountryTotalAttacks, numFatalities
 }
